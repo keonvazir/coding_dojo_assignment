bd9e47191ca1dd1f598f3321836fca698acae1ca
Keon Vazir <kvazir@Keons-MacBook-Air.local>
2019-09-18 14:23:41 -0700

    here you go Karl

--- Python/Django/login_proj/login_register/apps/login_app/models.py
+++ Python/Django/login_proj/login_register/apps/login_app/models.py
@@ -1,8 +1,16 @@
+from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45)
-    email = models.EmailField(max_length=255)
+    # email = models.EmailField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    confirm_password = models.CharField(max_length=255)
+    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
 
 # Create your models here.
--- Python/Django/login_proj/login_register/apps/login_app/views.py
+++ Python/Django/login_proj/login_register/apps/login_app/views.py
@@ -1,6 +1,11 @@
 ########## APP LEVEL views ###########
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+
+def root(request):
+    return redirect("/")
 
 def index(request):
     return render(request, "login_app/index.html")
