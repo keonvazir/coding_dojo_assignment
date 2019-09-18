9ac08d0aa11474b88c57d3c64af52567de26e7c4
Keon Vazir <kvazir@Keons-MacBook-Air.local>
2019-09-18 14:00:35 -0700

    sleep

--- Python/Django/login_proj/login_register/apps/login_app/models.py
+++ Python/Django/login_proj/login_register/apps/login_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField(max_length=255)
+
 # Create your models here.
