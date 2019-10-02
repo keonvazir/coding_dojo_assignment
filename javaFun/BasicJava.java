cb13515d49c241775f30a3ca856cefaeda932337
Keon Vazir <kvazir@Keons-MacBook-Air.local>
2019-10-01 22:29:18 -0700

    update

---
+++ javaFun/BasicJava.java
@@ -0,0 +1,75 @@
+// public class BasicJava{
+//     public static void main(String[] args){
+//         for(int i=0; i<255; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// public class BasicJava{
+//     public static void main(String[] args){
+//        int limit = 255; 
+//        for (int i=1; i <= limit; i++){
+//            if(i%2 != 0){
+//                System.out.println(i + "");
+//            }
+//        } 
+        
+//     }
+// }
+
+// public class BasicJava{
+//     public static void main(String[] args){
+//         int sum = 0;
+//         for(int i=0; i <= 255; i++){
+//             sum += i;
+//             System.out.println("New Number: " + i + "Sum: " + sum);
+//         }
+//     }
+// }
+
+// public class BasicJava{
+//     public static void main(String[] args){
+//         int[] intArray = new int[10];
+
+//         for(int i=0; i < intArray.length; i++) {
+//             intArray[i] = i;
+//         }
+
+//         for(int i=0; i < intArray.length; i++) {
+//             System.out.println( intArray[i] );
+//         }
+//     }
+// }
+
+// public class BasicJava{
+//     public static void main(String[] args){
+//         int arr[] = {10, 2, 3, 7};
+//         {
+//             int max = arr[0];
+//             for(int i = 1; i<arr.length; i++){
+//                 if(arr[i]>max){
+//                     max = arr[i];
+//                     
+//                 }
+               
+//             }
+//         }
+//         return max(10);
+//     }
+// }
+
+// public class BasicJava{
+//     public static void main(String[] args){
+//         public static int getMaxValue(int[] numbers){
+//         int maxValue = numbers[0];
+//         for(int i=1;i < numbers.length;i++){
+//             if(numbers[i] > maxValue){
+//             maxValue = numbers[i];
+//                 }
+//             }
+//              return maxValue;
+//         }
+//     }
+// }
+
