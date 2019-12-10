5c3cb4821ae660b1e4173ac23d6986b70aa02343
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-12-09 18:53:19 -0800

    add

--- Python/Django/roster_user_proj/apps/roster_user_app/views.py
+++ Python/Django/roster_user_proj/apps/roster_user_app/views.py
@@ -1,10 +1,20 @@
 ############ APP LEVEL ###############
 
-from django.shortcuts import render
-from . import views
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Roster, User
 
-urlpatterns = [
-    
-]
+def index(request):
+    context = {
+        "rosters": Roster.objects.all(),
+    }
+    return render(request, "roster_user_app/index.html", context)
 
-# Create your views here.
+def add_roster(request):
+    if request.method == "GET":
+        return redirect("/")
+    if request.method == "POST":
+        title = request.POST['roster_title']
+        description = request.POST['roster_description']
+        Roster.objects.create(title=title, description=description)
+        return redirect("/")
+
