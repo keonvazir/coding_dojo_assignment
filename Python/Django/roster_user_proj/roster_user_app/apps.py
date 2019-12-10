916f1ccf122fd0262dfbf4e9e8c75d3460a862b8
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-12-09 18:23:26 -0800

    intial

---
+++ Python/Django/roster_user_proj/roster_user_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RosterUserAppConfig(AppConfig):
+    name = 'roster_user_app'
---
+++ Python/Django/roster_user_proj/roster_user_app/views.py
@@ -0,0 +1,10 @@
+############ APP LEVEL ###############
+
+from django.shortcuts import render
+from . import views
+
+urlpatterns = [
+    
+]
+
+# Create your views here.
