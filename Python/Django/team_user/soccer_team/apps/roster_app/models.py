77f2a742d05f4cd25fa3f54603093bf2cbb74cf4
keonvazir <54293518+keonvazir@users.noreply.github.com>
2020-01-02 18:02:39 -0800

    models

--- Python/Django/team_user/soccer_team/apps/roster_app/models.py
+++ Python/Django/team_user/soccer_team/apps/roster_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Roster(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return f"<Roster object: {self.title} {self.description}>"
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    notes = models.TextField()
+    rosters = models.ManyToManyField(Roster, related_name="players")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __repr__(self):
+        return f"<Player object: {self.first_name} {self.last_name} {self.notes}>"
+
