89ffafe60eb2de84a2b9a5cde1524a10bbc2f68f
keonvazir <54293518+keonvazir@users.noreply.github.com>
2020-01-04 14:51:46 -0800

    view

--- Python/Django/team_user/soccer_team/apps/roster_app/urls.py
+++ Python/Django/team_user/soccer_team/apps/roster_app/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^players$', views.players_page),
+    url(r'^players/add_player', views.add_player),
+    url(r'^rosters/add_roster', views.add_roster),
+    url(r'players/(?P<player_id>\d+)$', views.one_player),
+    
+
 
 ]
--- Python/Django/team_user/soccer_team/apps/roster_app/views.py
+++ Python/Django/team_user/soccer_team/apps/roster_app/views.py
@@ -8,6 +8,55 @@ def index(request):
     }
     return render(request, "roster_app/index.html", context)
 
+def add_roster(request):
+    if request.method == "GET":
+        return redirect("/")
+    if request.method == "POST":
+        title = request.POST['roster_title']
+        description = request.POST['roster_description']
+        Roster.objects.create(title=title, description=description)
+        return redirect("/")
+
+    def players_page(request):
+        context = {
+            "players": Player.objects.all(),
+
+        }
+        return render(request, "roster_app/player.html", context)
+
+    def one_player(request, player_id):
+       player = Player.objects.get(id=player_id)
+       this_player_rosters = player.rosters.all()
+       all_rosters = Roster.objects.all()
+       other_rosters = [roster for roster in all_rosters if roster not in this_player_rosters]
+       context = {
+           "player": player,
+           "other_rosters": other_rosters,
+       }
+       return render(request, "roster_app/view_player.html", context)
+
+    def one_roster(request, roster_id):
+        roster = Roster.objects.get(id=roster_id)
+        this_roster_players = roster.players.all()
+        all_players = Player.objects.all()
+        other_players = [player for player in all_players if player not in this_roster_players]
+        context = {
+            "roster": roster,
+            "other_players": other_players,
+        }
+        return render(request, "roster_app/roster.html", context)
+
+    def add_player(request):
+        if request.method == "GET":
+            return redirect("/")
+        if request.method == "POST":
+            first_name = request.POST['player_first_name']
+            last_name = request.POST['player_last_name']
+            notes = request.POST['player_notes']
+            Player.objects.create(first_name=first_name, last_name=last_name, notes=notes)
+            return redirect("/players")
+
+
 
 
 
