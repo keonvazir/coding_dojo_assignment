6a3c3ab20547f9a576f19a6e536bdf58cf94ffd1
keonvazir <54293518+keonvazir@users.noreply.github.com>
2020-01-04 15:03:46 -0800

    templates

--- Python/Django/team_user/soccer_team/apps/roster_app/urls.py
+++ Python/Django/team_user/soccer_team/apps/roster_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^rosters/add_roster', views.add_roster),
     url(r'players/(?P<player_id>\d+)$', views.one_player),
     url(r'rosters/(?P<roster_id>\d+)$', views.one_roster),
+    url(r'rosters/(?P<roster_id>\d+)/add_player$', views.add_player_to_roster),
+    url(r'players/(?P<player_id>\d+)/add_roster$', views.add_roster_to_player),
 
 
 ]
